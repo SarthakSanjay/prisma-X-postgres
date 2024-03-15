8fb5c4ded4e5286f13fd76c1cdf42c967d42d045
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-15 19:29:53 +0530

    added comment test controllers

--- src/controllers/self.ts
+++ src/controllers/self.ts
@@ -20,3 +20,37 @@ export const getAllClients = async(req:Request, res:Response)=>{
     const clients = await prisma.client.findMany()
     res.status(200).json({clients})
 }
+
+export const createComment = async(req:Request ,res:Response)=>{
+    const {text ,cId} = req.body
+    const comment = await prisma.comment.create({
+        data:{
+            text:text,
+            parentId:cId
+        }
+    })
+    res.status(201).json({
+        msg:"commented on comment",
+        comment
+    })
+}
+
+export const getAllCommentOfComments = async(req:Request,res:Response)=>{
+    const commentId = req.body.commentId
+    const comment = await prisma.comment.findFirst({
+        where:{
+            id:commentId
+        },select:{
+            children:true
+        }
+    })
+    res.status(200).json({
+        comment
+    })
+}
+export const getAllComment = async(req:Request ,res:Response)=>{
+    const comments = await prisma.comment.findMany()
+    res.status(201).json({
+        comments
+    })
+}
